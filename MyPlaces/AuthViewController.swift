efc3081563d94796fb8b53138fbafc4fb8e60360
Marta Boteller <marta.boteller@gmail.com>
2018-11-25 22:45:42 +0100

    25Nov 3

--- MyPlaces/AuthViewController.swift
+++ MyPlaces/AuthViewController.swift
@@ -16,23 +16,47 @@ class AuthViewController: UIViewController {
     
     var myImage: Data = Data()
   
+    override func viewWillAppear(_ animated: Bool) {
+       
+     
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //Download task test
+        // - TEST -
+        //Download task 1
         let storage = Storage.storage()
         let globalReference = storage.reference()
         let ref = globalReference.child("demoJSON/demoPlaces.json")
-        let ref2 = globalReference.child("demoImages/5.png")
-        
-        let dic = PlaceServices.shared.downloadImage(reference: ref2)
-        // let data: Data = dic.keys.first!
+        let dic = PlaceServices.shared.downloadFile(reference: ref)
+        let data: [Place] = dic.keys.first!
         let downloadTask: StorageDownloadTask = dic.values.first!
+       
+       
+        //Download task 2
+        let ref2 = globalReference.child("demoImages/5.png")
+        let dic2 = PlaceServices.shared.downloadImage(reference: ref2)
+        let data2: Data = dic2.keys.first!
+        let downloadTask2: StorageDownloadTask = dic2.values.first!
+      
+        
         downloadTask.observe(.success) { snapshot in
-            // Download completed successfully
-            print("Downloaded successfully")
+            print("Download task 1 finished!")
+            print("Number of elements in place array \(data.count)")
         }
+        
+        downloadTask2.observe(.success) { snapshot in
+            print("Download task 2 finished!")
+            print("Data count \(data2.count)")
+            let docsPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+            let imageURL = docsPath.appendingPathComponent("test.png")
+            do{
+                try data2.write(to: imageURL)
+            } catch {
+                print (error)
+            }
+        }
+       
     }
     
   
