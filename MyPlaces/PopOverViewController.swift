8d06667cf02180aae1002096cf1cad4fb9dffd35
Marta Boteller <marta.boteller@gmail.com>
2018-12-10 08:06:59 +0100

    1

---
+++ MyPlaces/PopOverViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PopOverViewController.swift
+//  MyPlaces
+//
+//  Created by Marta Boteller on 3/12/18.
+//  Copyright © 2018 Marta Boteller. All rights reserved.
+//
+
+import UIKit
+
+
+class PopOverViewController: UIViewController, UIPopoverPresentationControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+  
+}
